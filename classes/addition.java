39f8d55afe8c7f90616462a07b75ff3ff1b24588
Piyush289kumar <piyushraikwar289@gmail.com>
2024-03-19 22:13:46 +0530

    java

---
+++ classes/addition.java
@@ -0,0 +1,16 @@
+package classes;
+
+/**
+ * main
+ */
+public class addition {
+    addition() {
+        super();
+        System.out.println("Constructor Called");
+    }
+
+    // main function
+    public static void main(String[] args) {
+        addition geek = new addition();
+    }System.out.println("Try programiz.pro");
+}
