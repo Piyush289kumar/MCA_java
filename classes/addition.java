921773183163f4709bf4ce70ccf35ed6a4b12606
Piyush289kumar <piyushraikwar289@gmail.com>
2024-03-19 22:20:11 +0530

    Java Class Done

--- classes/addition.java
+++ classes/addition.java
@@ -1,11 +1,12 @@
 package classes;
 
 class addition {
-    addition() {
-        System.out.println("Called");
+    addition(int val1, int val2) {
+        int res = val1 + val2;
+        System.out.println("Result : " + res);
     }
 
     public static void main(String[] args) {
-        addition obj = new addition();
+        addition obj = new addition(5, 2);
     }
 }
